9446a4dca88e567f15f89b7e570cbdc7d1adf34e
MoonGarc <elizamonse1@gmail.com>
2024-05-14 16:49:56 -0600

    HEX

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//Get the button element
+const btn = document.getElementById("btn");
+// Get the color element
+const color = document.querySelector(".color");
+
+//Add event Listener to the button 
+btn.addEventListener("click", ()=> {
+    //Obtener un numero aleatorio entre 0 y 3 
+    const randomNumber = getRandomNumber();
+    //Seleccionando un elemento 
+    //del arreglo colors
+    const colorPicked = colors[randomNumber]
+    //Cambiar el color del body 
+    document.body.style.backgroundColor = colorPicked;
+    //Cambiar el color de texto 
+    color.textContent = colorPicked;
+});
+
+//Funcion para obtener un número aleatorio 
+function getRandomNumber(){
+return Math.floor(Math.random() * colors.length );
+    }
--- hex.js
+++ hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Get the button element
+const btn = document.getElementById("btn");
+// Get the color element
+const color = document.querySelector(".color");
+
+// Add event Listener to the button
+btn.addEventListener("click", () => {
+    // Seleccionando un color hexadecimal aleatorio
+    const colorPicked = getRandomHexColor();
+    // Cambiar el color del body
+    document.body.style.backgroundColor = colorPicked;
+    // Cambiar el color de texto
+    color.textContent = colorPicked;
+});
+
+// Función para obtener un color hexadecimal aleatorio
+function getRandomHexColor() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    return hexColor;
+}
